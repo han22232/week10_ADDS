7137d842d1fffba32800bf91189c47891fe9bf03
han22232 <109938669+han22232@users.noreply.github.com>
2023-10-16 17:35:48 +1030

    Update klargest.cpp

--- klargest.cpp
+++ klargest.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 int kth_largest(vector<int> values, int k) {
-  priority_queue<int, vector<int>, greater<int>> pq;
+  priority_queue<int, vector<int>, greater<int>> pss;
   for (int i = 0; i < values.size(); i++) {
     pss.push(values.at(i));
     if (pss.size() > k) {
